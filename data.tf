b1acb5451c3d42c7597a0f2eac2300b959c4f516
Eswar <eswar@company.com>
2026-01-31 23:56:24 -0500

    Fix data source: query applications MG directly instead of from remote state

--- data.tf
+++ data.tf
@@ -3,16 +3,16 @@ data "terraform_remote_state" "landing_zone" {
   backend = "azurerm"
 
   config = {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatelzqiaypb"
-    container_name       = "tfstate"
-    key                  = "landingzone.tfstate"
+    resource_group_name  = var.landingzone_state_resource_group
+    storage_account_name = var.landingzone_state_storage_account
+    container_name       = var.landingzone_state_container
+    key                  = var.landingzone_state_key
   }
 }
 
 # Get Applications MG from landing zone to use as parent for app MGs
 data "azurerm_management_group" "applications" {
-  name = data.terraform_remote_state.landing_zone.outputs.applications_mg_id
+  name = "applications"
 }
 
 # Try to resolve app-specific management group
